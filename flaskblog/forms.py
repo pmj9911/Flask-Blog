175aefc9397aa2aebcfc45ce4835943cd0b9f078
pmj9911 <parthjardosh@gmail.com>
2019-07-04 16:28:05 +0530

    registration form working

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,16 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo ,ValidationError
+from flaskblog.models import User
 class RegistrationForm(FlaskForm):
 	username = StringField('Username' , validators = [DataRequired(), Length(min =2, max =20)]) 
 	email = StringField('Email',validators=[DataRequired(),		Email()])
 	password = PasswordField('password',validators=[DataRequired()])
-	confirm_password = PasswordField('confirm password',validators=[DataRequired(),	EqualTo('confirm_password')])
+	confirm_password = PasswordField('confirm password',validators=[DataRequired(),	EqualTo('password')])
 	submit = SubmitField('Sign Up')
 
+	def validate_email(self , email):
+
+		email = User.query.filter_by(email=email.data).first()
+		if email:
+			raise ValidationError('email already exists.')
+	def validate_username(self , username):
+
+		user = User.query.filter_by(username=username.data).first()
+		if user :
+			raise ValidationError('username already exists. Please choose another username')
 class LoginForm(FlaskForm):
 	email = StringField('Email',validators=[DataRequired(),	Email()])
 	password = PasswordField('password',validators=[DataRequired()])
-	confirm_password = PasswordField('confirm password',validators=[DataRequired(),	EqualTo('confirm_password')])
+	confirm_password = PasswordField('confirm password',validators=[DataRequired(),	EqualTo('password')])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
