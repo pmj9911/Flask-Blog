34b53c2c6ea4314c489e2ca48ab9e94e547cca21
pmj9911 <parthjardosh@gmail.com>
2019-07-06 16:27:34 +0530

    user profile update with picutre added

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo ,ValidationError
 from flaskblog.models import User
 from flask_login import current_user
+
 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username' , validators = [DataRequired(), Length(min =2, max =20)]) 
@@ -32,6 +34,7 @@ class LoginForm(FlaskForm):
 class UpdateAccountForm(FlaskForm):
 	username = StringField('Username' , validators = [DataRequired(), Length(min =2, max =20)]) 
 	email = StringField('Email',validators=[DataRequired(),		Email()])
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png','jpeg'])])
 	submit = SubmitField('Update')
 
 	def validate_email(self , email):
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,6 +3,10 @@ from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post 
 from flaskblog import app , db , bcrypt
 from flask_login import login_user, current_user,logout_user, login_required
+import secrets
+import os
+from PIL import Image
+
 
 posts = [
     {
@@ -70,12 +74,28 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _ , f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path=os.path.join(app.root_path,'static/profile_pics', picture_fn)
+    
+    output_size=(125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     # form = User.query.get(username = current_user.username)
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file=save_picture(form.picture.data)
+            current_user.image_file= picture_file
+
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
@@ -86,3 +106,10 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static' , filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+
+
+    return render_template('create_post.html', title='New Post')
